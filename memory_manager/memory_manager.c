3277d5db4ad21c887be361238c84f84c3b12b92c
Vibhu910 <68897616+Vibhu910@users.noreply.github.com>
2025-10-25 18:55:51 -0700

    Refactor address printing and clean up code
    Updated print functions to use %llx for 64-bit addresses and removed unused code for address translation.

--- memory_manager/memory_manager.c
+++ memory_manager/memory_manager.c
@@ -9,6 +9,7 @@
 #include <linux/highmem.h>
 #include <linux/swap.h>
 #include <linux/swapops.h>
+#include <linux/rcupdate.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("CSE330");
@@ -24,25 +25,26 @@ MODULE_PARM_DESC(addr, "Virtual address to translate (unsigned long)");
 
 static void print_phys(int pid_in, unsigned long vaddr, unsigned long phys_addr)
 {
-	printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%lx]  physical address [%lx] swap identifier [NA]\n",
-	       pid_in, vaddr, phys_addr);
+	printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx]  physical address [%llx] swap identifier [NA]\n",
+	       pid_in, (unsigned long long)vaddr, (unsigned long long)phys_addr);
 }
 
 static void print_swap(int pid_in, unsigned long vaddr, unsigned long swap_id)
 {
-	printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%lx]  physical address [NA] swap identifier [%lx]\n",
-	       pid_in, vaddr, swap_id);
+	printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx]  physical address [NA] swap identifier [%llx]\n",
+	       pid_in, (unsigned long long)vaddr, (unsigned long long)swap_id);
 }
 
 static void print_invalid(int pid_in, unsigned long vaddr)
 {
-	printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%lx]  physical address [NA] swap identifier [NA]\n",
-	       pid_in, vaddr);
+	printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx]  physical address [NA] swap identifier [NA]\n",
+	       pid_in, (unsigned long long)vaddr);
 }
 
 static int __init memory_manager_init(void)
 {
 	struct task_struct *task;
+	struct task_struct *found_task = NULL;
 	struct mm_struct *mm;
 	unsigned long address;
 	pgd_t *pgd;
@@ -53,98 +55,49 @@ static int __init memory_manager_init(void)
 	pte_t pte_entry;
 	bool found = false;
 
-	pr_info("[CSE330-Memory-Manager] module loaded: pid=%d addr=%lx\n", pid, addr);
-
 	if (pid < 0) {
-		pr_err("[CSE330-Memory-Manager] invalid pid parameter (must be non-negative)\n");
 		return -EINVAL;
 	}
 
 	address = (unsigned long)addr;
 
+	rcu_read_lock();
 	for_each_process(task) {
 		if (task->pid == pid) {
+			found_task = task;
 			found = true;
+			get_task_struct(found_task);
 			break;
 		}
 	}
+	rcu_read_unlock();
 
-	if (!found || !task) {
+	if (!found || !found_task) {
 		print_invalid(pid, address);
 		return 0;
 	}
 
-	mm = task->mm;
+	mm = found_task->mm;
 	if (!mm) {
+		put_task_struct(found_task);
 		print_invalid(pid, address);
 		return 0;
 	}
 
 	pgd = pgd_offset(mm, address);
 	if (!pgd || pgd_none(*pgd) || unlikely(pgd_bad(*pgd))) {
+		put_task_struct(found_task);
 		print_invalid(pid, address);
 		return 0;
 	}
 
 	p4d = p4d_offset(pgd, address);
 	if (!p4d || p4d_none(*p4d) || unlikely(p4d_bad(*p4d))) {
+		put_task_struct(found_task);
 		print_invalid(pid, address);
 		return 0;
 	}
 
 	pud = pud_offset(p4d, address);
 	if (!pud || pud_none(*pud) || unlikely(pud_bad(*pud))) {
-		print_invalid(pid, address);
-		return 0;
-	}
-
-	pmd = pmd_offset(pud, address);
-	if (!pmd || pmd_none(*pmd) || unlikely(pmd_bad(*pmd))) {
-		print_invalid(pid, address);
-		return 0;
-	}
-
-	pte_ptr = pte_offset_kernel(pmd, address);
-	if (!pte_ptr) {
-		print_invalid(pid, address);
-		return 0;
-	}
-
-	pte_entry = *pte_ptr;
-	if (pte_none(pte_entry)) {
-		print_invalid(pid, address);
-		return 0;
-	}
-
-	if (pte_present(pte_entry)) {
-		unsigned long pfn;
-		unsigned long phys_page_base;
-		unsigned long offset;
-		unsigned long phys_addr;
-		pfn = pte_pfn(pte_entry);
-		phys_page_base = (pfn << PAGE_SHIFT);
-		offset = address & ~PAGE_MASK;
-		phys_addr = phys_page_base | offset;
-		print_phys(pid, address, phys_addr);
-		return 0;
-	}
-
-	{
-		swp_entry_t swp = pte_to_swp_entry(pte_entry);
-		if (swp.val) {
-			print_swap(pid, address, (unsigned long)swp.val);
-		} else {
-			print_invalid(pid, address);
-		}
-	}
-
-	return 0;
-}
-
-static void __exit memory_manager_exit(void)
-{
-	pr_info("[CSE330-Memory-Manager] module unloaded\n");
-}
-
-module_init(memory_manager_init);
-module_exit(memory_manager_exit);
+		put_ta_
