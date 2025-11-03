e98b1c527cbd8db9e648f234b2ae8c692dfa8b8c
Vibhu910 <68897616+Vibhu910@users.noreply.github.com>
2025-11-02 22:46:43 -0700

    Refactor address handling in memory managernew

--- memory_manager/memory_manager.c
+++ memory_manager/memory_manager.c
@@ -23,6 +23,7 @@ static int __init memory_manager_init(void)
 	pud_t *pud;
 	pmd_t *pmd;
 	pte_t *pte;
+	unsigned long vaddr;
 
 	for_each_process(task) {
 		if (task->pid == pid) {
@@ -30,28 +31,33 @@ static int __init memory_manager_init(void)
 			if (!mm)
 				goto invalid;
 
-			pgd = pgd_offset(mm, addr);
+			vaddr = (unsigned long)addr;
+
+			pgd = pgd_offset(mm, vaddr);
 			if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd)))
 				goto invalid;
 
-			p4d = p4d_offset(pgd, addr);
+			p4d = p4d_offset(pgd, vaddr);
 			if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d)))
 				goto invalid;
 
-			pud = pud_offset(p4d, addr);
+			pud = pud_offset(p4d, vaddr);
 			if (pud_none(*pud) || unlikely(pud_bad(*pud)))
 				goto invalid;
 
-			pmd = pmd_offset(pud, addr);
+			pmd = pmd_offset(pud, vaddr);
 			if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd)))
 				goto invalid;
 
-			pte = pte_offset_kernel(pmd, addr);
+			pte = pte_offset_kernel(pmd, vaddr);
 			if (!pte)
 				goto invalid;
 
-			if (pte_present(*pte)) {
-				unsigned long long phys_addr = (pte_pfn(*pte) << PAGE_SHIFT) | (addr & ~PAGE_MASK);
+			if (pte_none(*pte)) {
+				printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx]  physical address [NA] swap identifier [NA]\n",
+				       pid, addr);
+			} else if (pte_present(*pte)) {
+				unsigned long long phys_addr = ((unsigned long long)pte_pfn(*pte) << PAGE_SHIFT) | (addr & ~PAGE_MASK);
 				printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx]  physical address [%llx] swap identifier [NA]\n",
 				       pid, addr, phys_addr);
 			} else {
